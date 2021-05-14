9f2deb3cd6793f2a0ab20e0a452945ac74396cc4
Chris Boyle <cthboyle6@gmail.com>
2021-05-14 11:45:14 -0400

    changed routes for root path to lists index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: 'lists#index'
+
+  resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
 
